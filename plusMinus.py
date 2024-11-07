e4d2786ba9c166b0513f5f9a5e1b62729eda4588
maxuel henrique paula campos <124100812+MaxxArtes@users.noreply.github.com>
2024-11-07 14:59:04 -0400

    plusMinus.py

---
+++ plusMinus.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    positivos = 0
+    negativos = 0
+    zeros = 0
+    for item in arr:
+        if item > 0:
+            positivos+=1
+        elif item < 0:
+            negativos+=1
+        else:
+            zeros+=1
+    proporcao_positivos = format((positivos / n), ".6f")
+    proporcao_negativos = format((negativos / n), ".6f")
+    proporcao_zeros = format((zeros / n), ".6f")
+    
+    return print(proporcao_positivos), print(proporcao_negativos), print(proporcao_zeros)
+    
+    
+if __name__ == '__main__':
+    n = int(input( 'digite a quantidade de elementos a serem colocados no seu array:').strip())
+
+    arr = list(map(int, input('digite o array de valores a serem calculado ex:(-4 3 -9 0 4 1): ').rstrip().split()))
+
+    plusMinus(arr)
